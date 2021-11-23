cf93edfeae875a0c7ffd38175014183229b4913c
Art <sergejm422@gmail.com>
2021-11-23 22:39:03 +0200

    first commit

---
+++ jsex.js
@@ -0,0 +1,53 @@
+//2. Создайте переменную num и присвойте ей значение 10. Выведите значение этой переменной с помощью console.log()
+
+let num = 10;
+console.log(num);
+
+//3. Создайте переменные first = 5 и second = 10. Выведите в console.log() их сумму, разность, произведение и частность (деление)
+let first = 5;
+let second =10;
+console.log(first + second);
+console.log(first * second);
+console.log(first - second);
+console.log(first / second);
+
+//4. Создайте переменные a = 3 и b = 10, их сумму присвойте в переменную result
+let a = 3;
+let b = 10;
+let result = a+b;
+
+/*5. Создайте переменную bar = 20 и qwerty = 5. В переменную res присвойте разность bar - qwerty и отнемите 10.
+В переменную end присвойте разность значения res и переменной qwerty
+*/
+let bar = 20;
+let qwerty = 2;
+let res = (bar - qwerty)-10;
+let end = res -qwerty;
+//6. Создайте переменную str и присвойте ей значение Hello, Palmo. Результат выведите в alert()
+let str = 'Hello Palmo';
+alert(str);
+
+// 7. Создайте переменную name с вашим имененем и age с возрастом. В alert() выведите результат Я (имя), мне (возраст). (значения подставить из переменных)
+let name = 'Сергей';
+let age = 23;
+alert('Я'+ (name) + 'мне'+ (age));
+
+// 8.Напишите скрипт, который считает количество секунд в часе, сутках и месяце. В console.log() выведите результат
+let seconds = 60;
+let hour = 60;
+let day = 24;
+let month = 30;
+
+console.log(seconds*hour*day*month);
+//9.Создайте переменную с числом и возведите в квадрат
+let y = 2;
+Math.pow(y, 2);
+
+//10.Напишите скрипт, который считает количество секунд в часе, сутках и месяце. В console.log() выведите результат
+let d = 8;
+let n = Math.pow(d,2);
+let x = Math.pow(d,3);
+
+
+
+
